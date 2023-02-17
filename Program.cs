74efe336673d0e289f5f7d0030fe5b56ff5d3b5c
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-17 11:26:32 +0530

    Added Program to Calculate Part Time Wage

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+            AddPartTime check = new AddPartTime(); 
+            check.CalculatePartime();
         }
     }
 }
