d44561994059f38e6f92928cae1d4dee6d81d8fa
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-17 10:24:45 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmpWageComputationWithOops
+    {
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
