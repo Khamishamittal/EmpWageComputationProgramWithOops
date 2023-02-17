2593643c3a92c0b4eaa294df06b0db2fbda7e981
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-17 11:36:34 +0530

    Added Program to Solve using Switch Case

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+            SwitchCase check= new SwitchCase();
+            check.UseSwitchCase();
         }
     }
 }
