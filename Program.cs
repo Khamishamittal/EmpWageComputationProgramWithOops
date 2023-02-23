9b7acd081fdf42c0d33c4dd08438b647cece80c6
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-23 11:52:52 +0530

    Create a Program to Save Total Wage of Each Company

--- Program.cs
+++ Program.cs
@@ -5,6 +5,15 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+          
+
+            EmpWageBuilder Samsung = new EmpWageBuilder("SAMSUNG", 20, 5, 25);
+            EmpWageBuilder Apple = new EmpWageBuilder("APPLE", 16, 6, 20);
+
+            Samsung.computeEmpWage();
+            Console.WriteLine(Samsung.toString());
+            Apple.computeEmpWage();
+            Console.WriteLine(Apple.toString());
         }
     }
-}
+    }
