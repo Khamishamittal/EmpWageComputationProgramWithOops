b5f08dcf0d76ddf3575f90d54640b8d22f766340
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-23 19:25:42 +0530

    Calculate Emp Wage of Multiple Companies using Interface

---
+++ IComputeEmgWage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageComputationWithOops
+{
+    public interface IComputeEmgWage
+    {
+
+        public void addCompanyEmpWage(string company, int empRate, int workingDays, int maxHours);
+    
+        public void computeEmpWage();
+    }
+
+    
+}
