f3754d75dc33bc24365c1181f9e3c00285342929
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-23 11:21:01 +0530

    Calculation of EmpWage of Multiple Companies

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+            MultipleCompanies Company = new MultipleCompanies();
+
+            Company.calculateWage("StarBucks", 15, 20, 60);
+
+            Company.calculateWage("Apple", 25, 22, 70);
+
+
         }
     }
 }
+    
