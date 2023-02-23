94014f5adf810a2b9a90cd29aca23c33c4df4b95
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-23 12:27:13 +0530

    Ability to manage Employee Wage of multiple companies

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+           
+
+            EmpWageBuilder wageBuilder = new EmpWageBuilder();
+
+            wageBuilder.addCompanyEmpWage("MICROSOFT", 20, 5, 20);
+            wageBuilder.addCompanyEmpWage("GOOGLE", 18, 4, 23);
+            wageBuilder.computeEmpWage();
         }
+
     }
-}
+    }
